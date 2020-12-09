3a331c81949e64b23718fb467a1ebaa91221cd58
William Lucid <11734452+Tech500@users.noreply.github.com>
2020-12-09 14:54:53 -0500

    Add files via upload

---
+++ index1.h
@@ -0,0 +1,21 @@
+//index1.h
+const char HTML1[] PROGMEM = R"====(
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>Sump Pit</title>
+    <meta http-equiv="refresh" content="30">
+</head>
+</head>
+
+<body>
+    <h1>Sump Pit Water Level</h1>
+    <h2>Distance to top of Sump Pit: %TOP% inches</h2>
+    <h2>%DATE%</h2>
+    <br> 
+    <br><h3> Client IP: %CLIENTIP%</h3>
+</body>
+
+</html>
+)====";
